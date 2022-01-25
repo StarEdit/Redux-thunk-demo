dde1ed4b0042a2c3fd7c730a2bfddbf56e90633f
StarEdit <71477444+StarEdit@users.noreply.github.com>
2022-01-25 15:18:49 +0700

    bank demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators, State } from "./redux/store";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+
+  const money = useSelector((state: State) => state.bank);
+
+  const [inputMoney, setInputMoney] = useState<number>(0);
+
+  const handleChangeInput = (e: any) => {
+    setInputMoney(parseInt(e.target.value));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{money}</h1>
+      <input type="text" value={inputMoney} onChange={handleChangeInput} />
+      <br />
+      <br />
+      <button onClick={() => depositMoney(inputMoney)}>DEPOSIT</button>
+      <button onClick={() => withdrawMoney(inputMoney)}>WITHDRAW</button>
+      <button onClick={() => bankrupt()}>BANKRUPT</button>
     </div>
   );
 }
